0b0738a2d3d090551489074a840f9fc6dae6dc06
unknown <mandlambolekwa@gmail.com>
2023-12-05 09:22:24 +0200

    displayed user data

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -4,19 +4,19 @@ export default function Portfolio({ userData }) {
   return (
     <div className="w-[80%] h-[80%] border rounded-md bg-white text-black">
       <div className="flex w-screen gap-5 p-10">
-        <div className="flex justify-around w-1/3 border-b-2 border-red-400 p-1 pl-5">
-          <span>Name</span>
-          <span>{userData.name}</span>
+        <div className="flex justify-around w-1/3 border-b-2 border-red-400">
+          <span className="bg-gray-300 w-screen rounded-sm p-1 text-center">Name</span>
+          <span className="bg-gray-400 w-screen rounded-sm p-1 text-gray-100 text-center">{userData.name}</span>
         </div>
-        <div className="flex justify-around w-1/3 border-b-2 border-red-400 p-1 pl-5">
-          <span>Surname</span>
-          <span>{userData.surname}</span>
+        <div className="flex justify-around w-1/3 border-b-2 border-red-400">
+          <span className="bg-gray-300 w-screen rounded-sm p-1 text-center">Surname</span>
+          <span className="bg-gray-400 w-screen rounded-sm p-1 text-gray-100 text-center">{userData.surname}</span>
         </div>
       </div>
       <div className="flex w-screen gap-5 p-10">
-        <div className="flex justify-around w-1/3 border-b-2 border-red-400 p-1 pl-5">
-          <span>Email address</span>
-          <span>{userData.email}</span>
+        <div className="flex justify-around w-1/3 border-b-2 border-yellow-400">
+          <span className="bg-gray-300 w-screen rounded-sm p-1 text-center">Email address</span>
+          <span className="bg-gray-400 w-screen rounded-sm p-1 text-gray-100 text-center">{userData.email}</span>
         </div>
       </div>
     </div>
