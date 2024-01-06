475b055203187f3693143dc2db4c1872953c6287
unknown <mandlambolekwa@gmail.com>
2024-01-06 14:19:54 +0200

    working on ui our team

--- src/components/Team.js
+++ src/components/Team.js
@@ -2,48 +2,32 @@
 
 import React, { useState } from "react";
 import ceo from "/public/business-man.png";
+import Image from "next/image";
 
 export default function Team() {
   const [isHovered, setIsHovered] = useState(false);
 
   return (
     <div className="bg-[#4a513f] text-white p-10">
-      <h1 className="text-center text-5xl mb-3">Our Team</h1>
-      <div className="grid grid-cols-3 gap-10 p-10">
-        <div
-          className="bg-[rgba(128,0,128,0.5)] p-4 border border-white rounded-lg flex items-center justify-center cursor-pointer"
-          style={{
-            backgroundImage: `url(${ceo.src})`,
-            backgroundSize: "cover",
-            backgroundRepeat: "no-repeat",
-            backgroundPosition: "center",
-          }}
-        >
-          <p className="relative bottom-0">CEO</p>
+      <h1 className="text-center text-5xl mb-10">Our Team</h1>
+      <div className="flex flex-row gap-10">
+        <div className="bg-[rgba(128,0,128,0.5)] p-4 border border-white flex flex-col items-center justify-center rounded-lg cursor-pointer w-full">
+          <Image alt="" src={ceo} width={150} height={150} />
+          <div className="mt-5">
+            <p className="text-4xl">CEO</p>
+          </div>
         </div>
-        <div
-          className={`bg-[rgba(128,0,128,0.5)] p-4 border border-white rounded-lg flex items-center justify-center cursor-pointer`}
-          style={{
-            backgroundImage: `url(${ceo.src})`,
-            backgroundSize: "cover",
-            backgroundRepeat: "no-repeat",
-            backgroundPosition: "center",
-          }}
-        >
-          {/*get ishovered state*/}
-
-          <p className="relative bottom-0">CEO</p>
+         <div className="bg-[rgba(128,0,128,0.5)] p-4 border border-white flex flex-col items-center justify-center rounded-lg cursor-pointer w-full">
+          <Image alt="" src={ceo} width={150} height={150} />
+          <div className="mt-5">
+            <p className="text-4xl">CEO</p>
+          </div>
         </div>
-        <div
-          className="bg-[rgba(128,0,128,0.5)] p-4 border border-white rounded-lg flex items-center justify-center cursor-pointer h-96"
-          style={{
-            backgroundImage: `url(${ceo.src})`,
-            backgroundSize: "cover",
-            backgroundRepeat: "no-repeat",
-            backgroundPosition: "center",
-          }}
-        >
-          <p className="relative bottom-0">CEO</p>
+         <div className="bg-[rgba(128,0,128,0.5)] p-4 border border-white flex flex-col items-center justify-center rounded-lg cursor-pointer w-full">
+          <Image alt="" src={ceo} width={150} height={150} />
+          <div className="mt-5">
+            <p className="text-4xl">CEO</p>
+          </div>
         </div>
       </div>
     </div>
