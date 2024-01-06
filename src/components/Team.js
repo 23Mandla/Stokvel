692088054130ea8261fb2d42178f30f2270ad0cd
unknown <mandlambolekwa@gmail.com>
2024-01-06 15:37:32 +0200

    working on ui our team

--- src/components/Team.js
+++ src/components/Team.js
@@ -1,28 +1,42 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 import ceo from "/public/business-man.png";
 import Image from "next/image";
 
-export default function Team() {
-  const [isHovered, setIsHovered] = useState(false);
+// Reducer function to handle state changes based on action types
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "CEO":
+      return { ...state, CEO: !state.CEO };
+    case "COO":
+      return { ...state, COO: !state.COO };
+    case "CTO":
+      return { ...state, CTO: !state.CTO };
+    default:
+      return state;
+  }
+};
 
-  const handleHover = () => {
-    setIsHovered((prevStae) => !prevStae);
-  };
+export default function Team() {
+  const [state, dispatch] = useReducer(reducer, {
+    CEO: false,
+    COO: false,
+    CTO: false,
+  });
 
   return (
     <div className="bg-[#4a513f] text-white p-10">
       <h1 className="text-center text-5xl mb-10">Our Team</h1>
-      <div className="flex flex-row gap-10">
+      <div className="flex flex-auto gap-10">
         <div
           className="bg-[rgba(128,0,128,0.5)] border border-white flex flex-col items-center justify-center rounded-lg cursor-pointer w-full overflow-hidden relative"
-          onMouseEnter={handleHover}
-          onMouseLeave={handleHover}
+          onMouseEnter={() => dispatch({ type: "CEO" })}
+          onMouseLeave={() => dispatch({ type: "CEO" })}
         >
           <div
             className={`absolute bg-[rgba(0,0,0,0.6)] h-full w-full transition-all duration-500 rounded-lg  ${
-              isHovered ? "right-0 opacity-100" : "right-full opacity-0"
+              state.CEO ? "right-0 opacity-100" : "right-full opacity-0"
             }`}
           >
             <h1>Mandla Mbolekwa</h1>
@@ -32,14 +46,15 @@ export default function Team() {
             <p className="text-4xl">CEO</p>
           </div>
         </div>
+
         <div
           className="bg-[rgba(128,0,128,0.5)] border border-white flex flex-col items-center justify-center rounded-lg cursor-pointer w-full overflow-hidden relative"
-          onMouseEnter={handleHover}
-          onMouseLeave={handleHover}
+          onMouseEnter={() => dispatch({ type: "COO" })}
+          onMouseLeave={() => dispatch({ type: "COO" })}
         >
           <div
             className={`absolute bg-[rgba(0,0,0,0.6)] h-full w-full transition-all duration-500 rounded-lg  ${
-              isHovered ? "right-0 opacity-100" : "right-full opacity-0"
+              state.COO ? "right-0 opacity-100" : "right-full opacity-0"
             }`}
           >
             <h1>Mandla Mbolekwa</h1>
@@ -49,14 +64,15 @@ export default function Team() {
             <p className="text-4xl">CEO</p>
           </div>
         </div>
+
         <div
           className="bg-[rgba(128,0,128,0.5)] border border-white flex flex-col items-center justify-center rounded-lg cursor-pointer w-full overflow-hidden relative"
-          onMouseEnter={handleHover}
-          onMouseLeave={handleHover}
+          onMouseEnter={() => dispatch({ type: "CTO" })}
+          onMouseLeave={() => dispatch({ type: "CTO" })}
         >
           <div
             className={`absolute bg-[rgba(0,0,0,0.6)] h-full w-full transition-all duration-500 rounded-lg  ${
-              isHovered ? "right-0 opacity-100" : "right-full opacity-0"
+              state.CTO ? "right-0 opacity-100" : "right-full opacity-0"
             }`}
           >
             <h1>Mandla Mbolekwa</h1>
